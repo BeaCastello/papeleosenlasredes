94f70a4671ee09de6f1a69f64b5794a2a58f40df
beatriz <designerbrm@gmail.com>
2024-11-13 23:22:03 +0100

    Nuevoscambios5

--- script.js
+++ script.js
@@ -53,8 +53,72 @@ function showSlide(index) {
 // Rotación imagen servicios 
 function toggleCardEffect(card) {
     if (!card.classList.contains('clicked')) {
-        card.classList.add('clicked'); // Añade la rotación
+        card.classList.add('clicked'); 
     } else {
-        card.classList.remove('clicked'); // Revierte la rotación
+        card.classList.remove('clicked'); 
     }
 }
+
+// Función whatsapp
+(function() {
+    emailjs.init("CPlHCPmFSQXzJ332c"); 
+})();
+
+
+// Mostrar y ocultar el modal
+const contactButton = document.getElementById("contactButton");
+const contactAlert = document.getElementById("contactAlert");
+const closeButton = document.querySelector(".close-button");
+
+contactButton.onclick = function() {
+    contactAlert.style.display = "flex";
+};
+
+closeButton.onclick = function() {
+    contactAlert.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == contactAlert) {
+        contactAlert.style.display = "none";
+    }
+};
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+     // Muestra tan solo una alerta
+     alert("Formulario enviado (simulación). Integra EmailJS o tu backend para enviar al correo.");
+     contactAlert.style.display = "none";
+ });
+ 
+ function openModal() {
+     document.getElementById("whatsappModal").style.display = "flex";
+ }
+ 
+ //cerar el modal
+ function closeModal() {
+     document.getElementById("whatsappModal").style.display = "none";
+ }
+ 
+ // Enviar mensaje al whasapp
+ function sendMessage(event) {
+     event.preventDefault();
+ 
+     // Obtener los datos del formulario
+     const name = document.getElementById("name").value;
+     const email = document.getElementById("email").value;
+     const message = document.getElementById("message").value;
+ 
+     
+     const whatsappMessage = `Hola, soy ${name}. Mi correo es ${email}. Mi consulta es: ${message}`;
+      
+     const phoneNumber = "642643599";
+ 
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+ 
+    window.open(whatsappURL, "_blank");
+ 
+    closeModal();
+ }
+
