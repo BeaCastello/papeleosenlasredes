008eaa127d049dc6b1bab637ad16187f933f8d07
beatriz <designerbrm@gmail.com>
2025-01-15 19:12:35 +0100

    Nuevos cambios

--- script.js
+++ script.js
@@ -77,6 +77,18 @@ document.getElementById('contactForm').addEventListener('submit', function (even
         });
 });
 
+// Conectando con el whatsapp
+const whatsappContainer = document.getElementById('whatsapp-container');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        whatsappContainer.classList.add('visible');
+    } else {
+        whatsappContainer.classList.remove('visible');
+    }
+});
+
+
 
 
 
